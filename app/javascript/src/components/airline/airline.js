a75a78b52dfadc19c14f2df4370ba58229ea34c9
Rob Sturcke <rob@rslabs.dev>
2020-12-31 16:58:01 -0500

    fixes stars, base setup of form with some minor issues - save point.

--- app/javascript/src/components/airline/airline.js
+++ app/javascript/src/components/airline/airline.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Review from './review';
 import Header from './review_header';
+import ReviewForm from './review_form';
+import AxiosHelper from '../../utils/requests/axios_helper';
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({});
@@ -29,6 +31,32 @@ const Airline = (props) => {
 
   const handleChange = (e) => {
     setReview(Object.assign({}, review, { [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    AxiosHelper();
+
+    const airline_id = parseInt(ariline.data.id);
+    axios
+      .post('/api/v1/reviews', { ...review, airline_id })
+      .then((resp) => {
+        setReviews([...reviews, resp.data.data]);
+        setReview({ title: '', description: '', score: 0 });
+        setError('');
+      })
+      .catch((resp) => {
+        let error;
+        switch (resp.message) {
+          case 'Request failed with status code 401':
+            error = 'Please log in to leave a review.';
+            break;
+          default:
+            error = 'Something went wrong.';
+        }
+        setError(error);
+      });
   };
 
   const setRating = (score, e) => {
@@ -64,10 +92,22 @@ const Airline = (props) => {
             average={average}
           />
           <section>
+            <div className="fixed_dropdown">
+              <ReviewForm
+                name={airline.data.attributes.name}
+                review={review}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                setRating={setRating}
+                error={error}
+              />
+            </div>
             <div className="row">
-              {userReviews
-                ? userReviews
-                : 'No Reviews Yet! Leave a review next time you fly!'}
+              <div className="col-sm-12">
+                {userReviews
+                  ? userReviews
+                  : 'No Reviews Yet! Leave a review next time you fly!'}
+              </div>
             </div>
           </section>
         </>
--- app/javascript/src/components/airline/review_form.js
+++ app/javascript/src/components/airline/review_form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const ReviewForm = (props) => {
   const ratingOverall = [5, 4, 3, 2, 1].map((score, index) => {
@@ -11,13 +12,38 @@ const ReviewForm = (props) => {
           name="rating"
           id={`rating-${score}`}
         />
+        <label onClick={props.setRating.bind(this, score)} />
       </>
     );
   });
 
   return (
     <>
-      <form>stuff</form>
+      <Form>
+        <Form.Group controlId="title">
+          <Form.Label>Review Title</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            placeholder="Your review title"
+            value={props.review.title}
+          />
+        </Form.Group>
+        <Form.Group controlId="description">
+          <Form.Label>Review description</Form.Label>
+          <Form.Control
+            type="text"
+            name="description"
+            placeholder="Your review's description"
+            value={props.review.description}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">{ratingOverall}</Form.Group>
+        <Button variant="primary" type="submit">
+          Submit Review
+        </Button>
+        {props.error && <p>{props.error}</p>}
+      </Form>
     </>
   );
 };
