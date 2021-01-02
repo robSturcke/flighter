66046f4f1c980b9fc0e51f11ced1911f76b47be7
Rob Sturcke <rob@rslabs.dev>
2021-01-01 19:20:39 -0500

    need to rethink rating component

--- app/javascript/src/components/airline/review_form.js
+++ app/javascript/src/components/airline/review_form.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-const ReviewForm = (props) => {
-  const ratingOverall = [5, 4, 3, 2, 1].map((score, index) => {
+const ReviewForm = (props, { value }) => {
+  const [rating, setRating] = useState(parseInt(value) || 0);
+  const [selection, setSelection] = useState(0);
+
+  const ratings = [5, 4, 3, 2, 1];
+
+  const handleHover = (event) => {
+    let val = 0;
+    if (event && event.target && event.target.getAttribute('star-id')) {
+      val = event.target.getAttribute('star-id');
+    }
+    setSelection(val);
+  };
+
+  const ratingOverall = ratings.map((score, index) => {
     return (
       <span key={index}>
-        <input
-          type="radio"
-          value={score}
-          checked={props.review.score == score}
-          name="rating"
-          id={`rating-${score}`}
-        />
-        <label onClick={props.setRating.bind(this, score)} />
+        <span star-id={score} className="rating">
+          <input
+            type="radio"
+            value={score}
+            checked={props.review.score == score}
+            onChange={() => console.log('onChange')}
+            onClick={(e) =>
+              setRating(e.target.getAttribute('star-id') || rating)
+            }
+            onMouseOver={handleHover}
+            name="rating"
+            id={`rating-${score}`}
+          />
+          <label onClick={props.setRating.bind(this, score)}></label>
+        </span>
       </span>
     );
   });
